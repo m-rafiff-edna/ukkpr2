6c491b25e29b60cb102ea34c85d7729f3fbb9800
m-rafif-edna <rafif.edna@gmail.com>
2025-11-24 11:08:43 +0700

    fix: notification icon for admin/petugas is now solid red circle

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -181,9 +181,17 @@
     <div class="hidden md:block fixed top-4 right-4 z-50">
         <div class="relative">
             <button type="button" id="notificationBtn" class="p-2 rounded-full text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 relative shadow-lg">
-                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                </svg>
+                @if(auth()->user()->role == 'admin' || auth()->user()->role == 'petugas')
+                    <!-- Solid red circle icon for admin/petugas -->
+                    <svg class="h-6 w-6" viewBox="0 0 24 24" fill="red" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="12" cy="12" r="10" />
+                    </svg>
+                @else
+                    <!-- Default notification icon for other roles -->
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                    </svg>
+                @endif
                 <span id="notificationBadge" class="hidden absolute top-0 right-0 block h-5 w-5 rounded-full bg-red-500 ring-2 ring-white text-white text-xs flex items-center justify-center font-bold"></span>
             </button>
         </div>
